c0ed9198e7520eec583864e05d767237495dccd0
yeji <skql4128@naver.com>
2023-06-12 13:57:01 +0900

    [🗒 Rename] 파일명 변경 #6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import GlobalStyle from './styles/GlobalStyles';
-import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import GlobalStyle from "./styles/GlobalStyles";
+import styled from "styled-components";
 // import MenuBar from './components/Common/MenuBar';
-import Home from '../src/pages/Home/Home';
-import Class from '../src/pages/Class/class';
-import Chatting from '../src/pages/Chatting/Chatting';
-import Posting from '../src/pages/Posting/posting';
-import Profile from '../src/pages/Profile/Profile';
+import Home from "../src/pages/Home/Home";
+import Class from "../src/pages/Class/Class";
+import Chatting from "../src/pages/Chatting/Chatting";
+import Posting from "../src/pages/Posting/Posting";
+import Profile from "../src/pages/Profile/Profile";
 function App() {
   return (
     <Wrap>
       <GlobalStyle />
       <Router>
         <Routes>
-          <Route exact path='/home*' element={<Home />} />
-          <Route exact path='/class*' element={<Class />} />
-          <Route exact path='/chat*' element={<Chatting />} />
-          <Route exact path='/post*' element={<Posting />} />
-          <Route exact path='/profile*' element={<Profile />} />
+          <Route exact path="/home*" element={<Home />} />
+          <Route exact path="/class*" element={<Class />} />
+          <Route exact path="/chat*" element={<Chatting />} />
+          <Route exact path="/post*" element={<Posting />} />
+          <Route exact path="/profile*" element={<Profile />} />
         </Routes>
       </Router>
     </Wrap>
