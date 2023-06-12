b5611fc7b8cf7e57cdb0315b1fd410789c270e5a
Da-Youn <dayoun202@gmail.com>
2023-06-12 11:52:03 +0900

    [🎨 Style] MenuBar 공통 컴포넌트 세팅작업 #2

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyles';
 import styled from 'styled-components';
-import MenuBar from './components/Common/MenuBar';
-import Home from './pages/Home/Home';
-import Class from './pages/Class/Class';
-import Chatting from './pages/Chatting/Chatting';
-import Posting from './pages/Posting/Posting';
-import Profile from './pages/Profile/Profile';
+// import MenuBar from './components/Common/MenuBar';
+import Home from '../src/pages/Home/Home';
+import Class from '../src/pages/Class/class';
+import Chatting from '../src/pages/Chatting/Chatting';
+import Posting from '../src/pages/Posting/posting';
+import Profile from '../src/pages/Profile/Profile';
 function App() {
   return (
     <Wrap>
       <GlobalStyle />
       <Router>
-        <MenuBar />
         <Routes>
-          <Route exact path='/home' component={Home} />
-          <Route exact path='/class' component={Class} />
-          <Route exact path='/chat' component={Chatting} />
-          <Route exact path='/post' component={Posting} />
-          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/home*' element={<Home />} />
+          <Route exact path='/class*' element={<Class />} />
+          <Route exact path='/chat*' element={<Chatting />} />
+          <Route exact path='/post*' element={<Posting />} />
+          <Route exact path='/profile*' element={<Profile />} />
         </Routes>
       </Router>
     </Wrap>
