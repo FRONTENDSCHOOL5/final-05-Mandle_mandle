28723927c09f655672fd4ab76e07b14aafc17ea9
yeji <skql4128@naver.com>
2023-06-13 09:53:18 +0900

    [🎨 Style] Style component 분리

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import Input from "../../components/Common/Input";
 import { ButtonStyle } from "../../components/Common/Button";
-import ClayImg from "../../assets/img/L-button-clay.svg";
 import ArrowImg from "../../assets/img/icon-arrow-left.svg";
+import {
+  TypeDiv,
+  ButtonImg,
+  SignupDiv,
+  TypeP,
+  Wrap,
+  LoginHeader,
+} from "./SignupStyle";
 
 export default function Signup() {
   return (
@@ -49,48 +56,3 @@ export default function Signup() {
     </SignupDiv>
   );
 }
-
-export const TypeDiv = styled.div`
-  display: flex;
-  gap: 13px;
-  margin: auto;
-  justify-content: center;
-`;
-export const ButtonImg = styled.button`
-  display: block;
-  width: 350px;
-  height: 70px;
-  margin: 17px 0 0 -15px;
-  background: url(${ClayImg}) center / contain no-repeat;
-`;
-export const SignupDiv = styled.div`
-  margin: auto;
-  padding: auto;
-  max-width: 390px;
-`;
-
-export const TypeP = styled.p`
-  font-size: 12px;
-  color: #767676;
-  margin-top: 10px;
-  margin-bottom: 10px;
-`;
-
-export const Wrap = styled.div`
-  width: 322px;
-  margin: auto;
-`;
-
-export const LoginHeader = styled.header`
-  display: flex;
-  position: relative;
-  justify-content: center;
-  align-items: center;
-  padding: 33px 34px 40px;
-
-  button {
-    position: absolute;
-
-    left: 34px;
-  }
-`;
--- src/pages/Signup/SignupStyle.jsx
+++ src/pages/Signup/SignupStyle.jsx
@@ -1,1 +1,49 @@
-// 파일 생성
+import React from "react";
+import styled from "styled-components";
+import GlobalStyleComponent from "../../styles/GlobalStyles";
+import ClayImg from "../../assets/img/L-button-clay.svg";
+
+export const TypeDiv = styled.div`
+  display: flex;
+  gap: 13px;
+  margin: auto;
+  justify-content: center;
+`;
+export const ButtonImg = styled.button`
+  display: block;
+  width: 350px;
+  height: 70px;
+  margin: 17px 0 0 -15px;
+  background: url(${ClayImg}) center / contain no-repeat;
+`;
+export const SignupDiv = styled.div`
+  margin: auto;
+  padding: auto;
+  max-width: 390px;
+`;
+
+export const TypeP = styled.p`
+  font-size: 12px;
+  color: var(--sub-font-color);
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Wrap = styled.div`
+  width: 322px;
+  margin: auto;
+`;
+
+export const LoginHeader = styled.header`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  padding: 33px 34px 40px;
+
+  button {
+    position: absolute;
+    left: 34px;
+  }
+`;
+export { TypeDiv, ButtonImg, SignupDiv, TypeP, Wrap, LoginHeader };
