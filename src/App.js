759e078c66d8328f3836f79c362a51b6639c0ab9
junny97 <pcohad12@gmail.com>
2023-06-13 17:44:02 +0900

    [✨Feat] App.js에 Router연결

--- src/App.js
+++ src/App.js
@@ -10,15 +10,23 @@ import Class from "../src/pages/Class/Class";
 import Chatting from "../src/pages/Chatting/Chatting";
 import Posting from "../src/pages/Posting/Posting";
 import Profile from "../src/pages/Profile/Profile";
+import Login from "../src/pages/Login/Login";
+import Signup from "../src/pages/Signup/Signup";
+import Loading from "../src/pages/Loading/Loading";
+import Intro from "../src/pages/Intro/Intro";
 
 function App() {
   return (
     <Wrap>
       <GlobalStyle />
-
+      {/* <Login /> */}
       <Router>
         <Routes>
+          <Route path="/" element={<Loading />} />
+          <Route path="/intro" element={<Intro />} />
           <Route path="/home" element={<Home to="/home/search" />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
           <Route path="/home/search" element={<Search />} />
           <Route path="/home/postdetail" element={<PostDetail />} />
           <Route exact path="/class*" element={<Class />} />
