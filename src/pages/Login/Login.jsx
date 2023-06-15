eef03c23224a69c2d09812692bc34e01bd66639d
junny97 <pcohad12@gmail.com>
2023-06-15 14:09:31 +0900

    [🔨 Refactor] 로그인 기능 유효성 검사 리팩토링 - 로그인 유효성 검사하는 이벤트 onchange에서 onBlur로 변경

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import styled from 'styled-components';
-import { React, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { tokenState } from '../../Store/userInfoAtoms';
-import ArrowImg from '../../assets/img/icon-arrow-left.svg';
-import DisabledButtonImg from '../../assets/img/L-Disabled-button(clay).svg';
-import ButtonImg from '../../assets/img/L-button(clay).svg';
+import axios from "axios";
+import styled from "styled-components";
+import { React, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { tokenState } from "../../Store/userInfoAtoms";
+import ArrowImg from "../../assets/img/icon-arrow-left.svg";
+import DisabledButtonImg from "../../assets/img/L-Disabled-button(clay).svg";
+import ButtonImg from "../../assets/img/L-button(clay).svg";
 export default function Login() {
   const [token, setToken] = useRecoilState(tokenState);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailErrorMessage, setEmailErrorMessage] = useState('');
-  const [pwErrorMessage, setPwErrorMessage] = useState('');
-  const [loginErrorMessage, setLoginErrorMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [pwErrorMessage, setPwErrorMessage] = useState("");
+  const [loginErrorMessage, setLoginErrorMessage] = useState("");
   const [buttonImg, setButtonImg] = useState(DisabledButtonImg);
   const navigate = useNavigate();
 
@@ -26,9 +26,9 @@ export default function Login() {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     // 입력란 값 변경
-    if (name === 'email') {
+    if (name === "email") {
       setEmail(value.trim());
-    } else if (name === 'password') {
+    } else if (name === "password") {
       setPassword(value.trim());
     }
 
@@ -36,8 +36,30 @@ export default function Login() {
     // 두 입력란에 값이 모두 존재할 경우 버튼 활성화 함수 실행
   };
 
+  const handleEmailBlur = () => {
+    const emailPattern = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if (!emailPattern.test(email)) {
+      setEmailErrorMessage("*올바른 이메일 양식을 입력해주세요");
+    } else {
+      setEmailErrorMessage("");
+    }
+  };
+
+  const handlePwordBlur = () => {
+    const passwordPattern =
+      /^(?=.*[!@#$%^&()_+=-])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    if (!passwordPattern.test(password)) {
+      setPwErrorMessage(
+        "*비밀번호는 8자 이상 특수문자와 대문자를 포함해야 합니다!"
+      );
+      return;
+    } else {
+      setPwErrorMessage("");
+    }
+  };
+
   const handleActiveButton = () => {
-    if (email !== '' && password !== '') {
+    if (email !== "" && password !== "") {
       setButtonImg(ButtonImg);
     } else {
       setButtonImg(DisabledButtonImg);
@@ -46,25 +68,6 @@ export default function Login() {
 
   const handleLoginSubmit = async (event) => {
     event.preventDefault();
-
-    //정규 표현식을 사용한 이메일, 비밀번호 유효성 검사
-    const emailPattern = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-    if (!emailPattern.test(email)) {
-      setEmailErrorMessage('*올바른 이메일 양식을 입력해주세요');
-    } else {
-      setEmailErrorMessage('');
-    }
-
-    const passwordPattern =
-      /^(?=.*[!@#$%^&()_+=-])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-    if (!passwordPattern.test(password)) {
-      // setPwErrorMessage(
-      //   '*비밀번호는 8자 이상 특수문자와 대문자를 포함해야 합니다!',
-      // );
-      // return;
-    } else {
-      setPwErrorMessage('');
-    }
 
     try {
       const data = {
@@ -75,20 +78,20 @@ export default function Login() {
       };
       console.log(data);
       const response = await axios.post(
-        'https://api.mandarin.weniv.co.kr/user/login',
+        "https://api.mandarin.weniv.co.kr/user/login",
         data,
         {
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-        },
+        }
       );
 
       if (response.data.status === 422) {
-        setLoginErrorMessage('*이메일  또는 비밀번호가 일치하지 않습니다.');
+        setLoginErrorMessage("*이메일  또는 비밀번호가 일치하지 않습니다.");
       } else if (response.data.user) {
         setToken(response.data.user.token);
-        navigate('/home');
+        navigate("/home");
       }
     } catch (error) {
       console.error(error);
@@ -99,7 +102,7 @@ export default function Login() {
     <LoginWrap>
       <LoginHeader>
         <button onClick={goBack}>
-          <img src={ArrowImg} alt='' />
+          <img src={ArrowImg} alt="" />
         </button>
         <h1>로그인</h1>
       </LoginHeader>
@@ -107,14 +110,15 @@ export default function Login() {
         <InputDiv>
           <Label>이메일</Label>
           <InputBox
-            className='email-input'
-            name='email' // 입력란 식별을 위해 name 속성 추가
-            width='322px'
-            height='48px'
-            padding='15px'
+            className="email-input"
+            name="email" // 입력란 식별을 위해 name 속성 추가
+            width="322px"
+            height="48px"
+            padding="15px"
             onChange={handleInputChange}
-            placeholder='이메일을 입력해주세요'
-            brColor={emailErrorMessage ? 'var(--error-color)' : '#dbdbdb'}
+            onBlur={handleEmailBlur} // 수정: onBlur 이벤트 핸들러 추가
+            placeholder="이메일을 입력해주세요"
+            brColor={emailErrorMessage ? "var(--error-color)" : "#dbdbdb"}
           />
         </InputDiv>
 
@@ -123,25 +127,26 @@ export default function Login() {
         <InputDiv>
           <Label>비밀번호</Label>
           <InputBox
-            className='pw-input'
-            name='password' // 입력란 식별을 위해 name 속성 추가
-            width='322px'
-            height='48px'
+            className="pw-input"
+            name="password" // 입력란 식별을 위해 name 속성 추가
+            width="322px"
+            height="48px"
             onChange={handleInputChange}
-            type='password'
-            placeholder='비밀번호를 입력하세요'
-            brColor={pwErrorMessage ? 'red' : '#dbdbdb'}
+            type="password"
+            onBlur={handlePwordBlur}
+            placeholder="비밀번호를 입력하세요"
+            brColor={pwErrorMessage ? "red" : "#dbdbdb"}
           />
         </InputDiv>
         {(pwErrorMessage && <ErrorMessage>{pwErrorMessage}</ErrorMessage>) ||
           (loginErrorMessage && (
             <ErrorMessage>{loginErrorMessage}</ErrorMessage>
           ))}
-        <button type='submit'>
-          <img src={buttonImg} alt='' />
+        <button type="submit">
+          <img src={buttonImg} alt="" />
         </button>
       </LoginForm>
-      <MoveSingUp to='/signup'>이메일로 회원가입</MoveSingUp>
+      <MoveSingUp to="/signup">이메일로 회원가입</MoveSingUp>
     </LoginWrap>
   );
 }
