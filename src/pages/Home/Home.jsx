5aa47539da42d485639fc366fb0f160e416ed2df
Da-Youn <dayoun202@gmail.com>
2023-06-18 02:28:26 +0900

    [✨Feat] 팔로잉 게시글 목록 받아오기 #93

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import HomeLogo from "../../assets/img/home-logo.svg";
-import MenuBar from "../../components/Common/MenuBar";
-import ButtonStyle from "../../components/Common/Button";
-import { HomeNav } from "../../components/Common/TopNav";
-import PostList from "../../components/Common/PostList/PostList";
-import { UserAtom } from "../../Store/userInfoAtoms";
-import { useRecoilState } from "recoil";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import MenuBar from '../../components/Common/MenuBar';
+import { HomeNav } from '../../components/Common/TopNav';
+import { UserAtom } from '../../Store/userInfoAtoms';
+import { useRecoilState } from 'recoil';
+import PostList from '../../components/Common/PostList/PostList';
+import GetFollowPost from '../../api/GetFollowPost';
+import { Link } from 'react-router-dom';
+import HomeLogo from '../../assets/img/home-logo.svg';
+import ButtonStyle from '../../components/Common/Button';
+
 export default function Home({ to }) {
-  const token = useRecoilState(UserAtom);
+  const [userValue] = useRecoilState(UserAtom); // UserAtom값 불러오기
+  const token = userValue.token; // token값 추출
+  const [postData, setPostData] = useState(null);
+  const [postCount, setPostCount] = useState(5);
+
+  useEffect(() => {
+    const response = async () => {
+      const data = await GetFollowPost(postCount, token);
+      setPostData(data);
+      console.log(postData);
+    };
+
+    response();
+  }, [postCount, postData, token]);
+
   console.log(token);
+  console.log(postData);
+
   return (
     <HomeWrap>
-      <HomeNav title="홈" to={to}></HomeNav>
+      <HomeNav title='홈' to={to}></HomeNav>
       <MainWrap>
-        {/* <img src={HomeLogo} alt='' />
-        <p>유저를 검색해 팔로우 해보세요!</p>
-        <Link to={to}>
-          <ButtonStyle
-            width='120px'
-            height='44px'
-            color='#fff'
-            bg='#036635'
-            br='44px'
-          >
-            검색하기
-          </ButtonStyle>
-        </Link> */}
-        {
+        {postData === null || postData.length === 0 ? (
+          <>
+            <img src={HomeLogo} alt='' />
+            <p>유저를 검색해 팔로우 해보세요!</p>
+            <Link to={to}>
+              <ButtonStyle
+                width='120px'
+                height='44px'
+                color='#fff'
+                bg='#036635'
+                br='44px'
+              >
+                검색하기
+              </ButtonStyle>
+            </Link>
+          </>
+        ) : (
           <ul>
             <PostList />
             <PostList />
           </ul>
-        }
+        )}
       </MainWrap>
       <MenuBar />
     </HomeWrap>
@@ -53,6 +74,7 @@ const MainWrap = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 16px;
   width: 100%;
   height: calc(100% - 48px - 60px);
   padding: 0 16px;
