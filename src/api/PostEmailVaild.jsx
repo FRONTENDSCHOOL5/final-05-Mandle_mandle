3274cb278665e4e12579cffba9ec02dedc526bc5
junny97 <pcohad12@gmail.com>
2023-06-17 17:41:13 +0900

    [🔨 Refactor] 회원가입/로그인 api 파일 생성 후 API전송 코드 분리

---
+++ src/api/PostEmailVaild.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export default async function PostEmailValid(value) {
+  try {
+    const response = await axios.post(
+      "https://api.mandarin.weniv.co.kr/user/emailvalid",
+      {
+        user: {
+          email: value,
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const json = response.data;
+    console.log(json.message);
+    return json.message;
+  } catch (error) {
+    console.error(error);
+  }
+}
