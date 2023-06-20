8ac53eb7acc29671191ffa44e273a08edd21d59f
Da-Youn <dayoun202@gmail.com>
2023-06-21 02:33:38 +0900

    [✨Feat] 게시글 상세 API 설정 및 연결 #110

---
+++ src/api/GetPostDetail.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default async function GetPostDetail(postId, token) {
+  try {
+    const response = await axios.get(
+      `https://api.mandarin.weniv.co.kr/post/${postId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      },
+    );
+    // data 객체 중 post 객체 받아오기
+    console.log(response);
+    return response.data.post;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
