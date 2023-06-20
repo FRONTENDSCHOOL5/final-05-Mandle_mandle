9dbb7bac9282b8fcda7762ca6dcfabad58eb7450
junny97 <pcohad12@gmail.com>
2023-06-20 15:21:46 +0900

    [📝 Docs] 이미지등록하기 API파일 분리

--- src/api/PostUpload.jsx
+++ src/api/PostUpload.jsx
@@ -1,5 +1,27 @@
 import axios from "axios";
+export const PostUpload = async (files) => {
+  const formData = new FormData();
+  for (let i = 0; i < files.length; i++) {
+    formData.append("image", files[i]);
+  }
 
-export const PostUpload = (formData) => {
-  return axios.post("/image/uploadfiles", formData);
+  try {
+    const response = await axios.post(
+      "https://api.mandarin.weniv.co.kr/image/uploadfiles",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    const filenames = response.data.map((image) => image.filename);
+    const mergedFilenames = filenames.join(",");
+
+    return mergedFilenames;
+  } catch (error) {
+    console.error("Image upload failed:", error);
+    throw error;
+  }
 };
