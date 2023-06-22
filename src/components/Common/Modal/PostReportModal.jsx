eb3894ed3486d4e2643977f0782ece44667768ee
Da-Youn <dayoun202@gmail.com>
2023-06-22 09:43:49 +0900

    [✨Feat] ModalAlert 컴포넌트 띄우기, 취소기능 구현, 삭제기능 연결 #117

--- src/components/Common/Modal/PostReportModal.jsx
+++ src/components/Common/Modal/PostReportModal.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { UserAtom } from '../../../Store/userInfoAtoms';
 import { useRecoilValue } from 'recoil';
 import PostReportPost from '../../../api/PostReportPost';
+import { ModalOverlay, ModalWrap } from './ModalStyle';
 export default function PostReportModal({ data, setModalOpen, category }) {
   const userInfo = useRecoilValue(UserAtom);
   const token = userInfo.token;
@@ -32,61 +33,3 @@ export default function PostReportModal({ data, setModalOpen, category }) {
     </ModalOverlay>
   );
 }
-
-const ModalOverlay = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  z-index: 9999; /* 모달의 z-index 값을 설정 */
-`;
-
-const ModalWrap = styled.div`
-  width: 100%;
-  padding-bottom: 6px;
-  position: absolute;
-  bottom: 0;
-  left: 50%;
-  transform: translate(-50%, 0);
-  border-radius: 10px 10px 0 0;
-  background-color: #fff;
-
-  p {
-    font-size: var(--font-lg);
-    padding: 24px 0;
-    border-bottom: 1px solid var(--border-color);
-  }
-  div {
-    width: 100%;
-    position: relative;
-    padding: 16px 0;
-  }
-
-  div::after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 50px;
-    height: 4px;
-    background: #dbdbdb;
-    border-radius: 5px;
-  }
-
-  ul {
-    width: 100%;
-  }
-  li {
-    width: 100%;
-    height: 46px;
-    padding-left: 26px;
-    display: flex;
-    align-items: center;
-  }
-`;
