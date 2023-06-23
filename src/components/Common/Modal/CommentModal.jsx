ca7a1791b00d02f4e171fb94ad1f485f96990714
Da-Youn <dayoun202@gmail.com>
2023-06-23 10:32:21 +0900

    [🎨 Style] CommentModal 불필요한 import 정리 #117

--- src/components/Common/Modal/CommentModal.jsx
+++ src/components/Common/Modal/CommentModal.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import { ModalOverlay, ModalWrap } from './ModalStyle';
-import ModalAlert from './ModalAlert/ModalAlert';
-import { useNavigate } from 'react-router-dom';
+
 export default function CommentModal({ setModalOpen, setAlertModalOpen }) {
   const handleOverlayClick = (event) => {
     if (event.target === event.currentTarget) {
