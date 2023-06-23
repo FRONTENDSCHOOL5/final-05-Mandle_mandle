e8239a3e6fe7b580a2c3c4eab80fb30f48cc690f
Da-Youn <dayoun202@gmail.com>
2023-06-24 00:11:45 +0900

    [🔨 Refactor] 스타일 컴포넌트 분리

--- src/pages/Home/PostDetail/PostDetailStyle.jsx
+++ src/pages/Home/PostDetail/PostDetailStyle.jsx
@@ -1,26 +1,25 @@
 import styled from 'styled-components';
-export const HomeWrap = styled.div`
+export const PostDetailWrap = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
-  font-size: var(--font-md);
 `;
 
 export const MainWrap = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 16px;
   width: 100%;
   height: calc(100% - 48px - 60px);
   padding: 0 16px;
-`;
-export const PostUlStyle = styled.ul`
-  width: 100%;
+  border-bottom: 1px solid var(--border-color);
   overflow-y: scroll;
-
   &::-webkit-scrollbar {
     display: none;
   }
+`;
+
+export const CommentUl = styled.ul`
+  padding-top: 12px;
+  width: 100%;
+  border-top: 1px solid var(--border-color);
+  gap: 12px;
 `;
