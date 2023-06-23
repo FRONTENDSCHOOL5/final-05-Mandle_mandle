b629d9bbb939f4d62234a80a34d343c348a825fb
Da-Youn <dayoun202@gmail.com>
2023-06-23 10:16:14 +0900

    [🖌 Design] PostBlank 컴포넌트 레이아웃 스타일 추가  #96

--- src/pages/Home/PostBlank/PostBlank.jsx
+++ src/pages/Home/PostBlank/PostBlank.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import HomeLogo from '../../../assets/img/home-logo.svg';
 import ButtonStyle from '../../../components/Common/Button';
-
-export default function PostBlank({ to }) {
+import { PostBlankWrap } from './PostBlankStyle';
+export default function PostBlank() {
   return (
-    <>
+    <PostBlankWrap>
       <img src={HomeLogo} alt='' />
       <p>유저를 검색해 팔로우 해보세요!</p>
       <Link to='/home/search'>
@@ -19,6 +20,6 @@ export default function PostBlank({ to }) {
           검색하기
         </ButtonStyle>
       </Link>
-    </>
+    </PostBlankWrap>
   );
 }
