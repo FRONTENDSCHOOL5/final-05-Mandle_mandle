2334b61c31f60d46c6d53b5537226e15101f7c00
Da-Youn <dayoun202@gmail.com>
2023-06-25 14:58:48 +0900

    [🖌 Design] Loading 애니메이션 효과 추가 및 스타일 컴포넌트 분리

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,51 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import LoadingImg from '../../assets/img/full-logo(loading).svg';
-import LoadedImg from '../../assets/img/full-logo(loaded).svg';
-import LogoImg from '../../assets/img/logo.svg';
-import styled from 'styled-components';
+
 import { useNavigate } from 'react-router-dom';
-const Loading = () => {
-  const [isLoading, setIsLoading] = useState(true);
+import LogoImg from '../../assets/img/logo.svg';
+import LoadedImg from '../../assets/img/full-logo(loaded).svg';
+import LoadingImg from '../../assets/img/full-logo(loading).svg';
+import { useRecoilValue } from 'recoil';
+import { UserAtom } from '../../Store/userInfoAtoms';
+import { LoadingWrap } from './LoadingStyle';
+export default function Loading() {
+  const userInfo = useRecoilValue(UserAtom);
+  const [isLoaded, setIsLoaded] = useState(false);
   const navigate = useNavigate();
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false);
+      setIsLoaded(true);
     }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
 
   useEffect(() => {
-    if (!isLoading) {
+    if (isLoaded) {
       const introTimer = setTimeout(() => {
-        navigate('/intro');
-      }, 2000);
+        if (userInfo) {
+          navigate('/home');
+        } else {
+          navigate('/intro');
+        }
+      }, 3000);
 
       return () => clearTimeout(introTimer);
     }
-  }, [isLoading, navigate]);
+  }, [isLoaded, userInfo, navigate]);
 
   return (
     <LoadingWrap>
-      {isLoading ? (
-        <img src={LoadingImg} alt='Loading' />
+      {isLoaded ? (
+        <img src={LoadedImg} className='twist' alt='Loaded' />
       ) : (
-        <img src={LoadedImg} alt='Loaded' />
+        <img src={LoadingImg} className='bounce' alt='Loading' />
       )}
       <h1>
-        <img src={LogoImg} alt='' />
+        <img src={LogoImg} className={isLoaded && 'twist'} alt='' />
       </h1>
     </LoadingWrap>
   );
-};
-
-const LoadingWrap = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-export default Loading;
+}
