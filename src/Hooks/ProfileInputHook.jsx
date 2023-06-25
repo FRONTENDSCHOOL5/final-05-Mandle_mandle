c6569590dfb56ff41ed849e1797b3be3721e70e9
junny97 <pcohad12@gmail.com>
2023-06-25 20:52:17 +0900

    [✨Feat] input값 유효성검사 커스텀훅으로 분리  #153

---
+++ src/Hooks/ProfileInputHook.jsx
@@ -0,0 +1,35 @@
+import { React, useState, useEffect } from 'react';
+import DisabledButtonImg from '../assets/img/L-Disabled-button(clay).svg';
+import ClayButtonImg from '../assets/img/L-start-button(clay).svg';
+export default function ProfileInputHook() {
+  const [inputValues, setInputValues] = useState({
+    username: '',
+    accountname: '',
+    intro: '',
+  });
+
+  const [buttonImg, setButtonImg] = useState(DisabledButtonImg);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [name]: value.trim(),
+    }));
+  };
+
+  useEffect(() => {
+    handleActiveButton();
+  }, [inputValues]);
+
+  const handleActiveButton = () => {
+    const { username, accountname, intro } = inputValues;
+    if (username !== '' && accountname !== '' && intro !== '') {
+      setButtonImg(ClayButtonImg);
+    } else {
+      setButtonImg(DisabledButtonImg);
+    }
+  };
+
+  return [inputValues, handleInputChange, buttonImg, setButtonImg];
+}
