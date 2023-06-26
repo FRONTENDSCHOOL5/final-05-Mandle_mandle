0b8130bc62bbe629490859ee8fc5ad967bccb022
Da-Youn <dayoun202@gmail.com>
2023-06-27 00:33:06 +0900

    [🔨 Refactor] 불필요한 import 제거

--- src/components/Common/PostList/PostList.jsx
+++ src/components/Common/PostList/PostList.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { FollowPostAtom } from '../../../Store/FollowPostAtom';
-import { useRecoilValue } from 'recoil';
-
 import PostContent from './PostContent';
 import PostProfile from './PostProfile';
 export default function PostList({ post, setPostUpdated }) {
-  if (!post) {
-    // post가 undefined인 경우 로딩 상태 등을 처리할 수 있습니다.
-    return <div>Loading...</div>;
-  }
   return (
     <PostLiStyle>
       <PostProfile setPostUpdated={setPostUpdated} post={post} />
