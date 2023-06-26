f1c324c6f99ab33621037a765a29499b53f9571e
junny97 <pcohad12@gmail.com>
2023-06-26 17:19:53 +0900

    [🖌 Design] 게시물 이미지 box-shadow 속성 변경

--- src/pages/Posting/PostingStyle.jsx
+++ src/pages/Posting/PostingStyle.jsx
@@ -164,9 +164,7 @@ export const ImagePreview = styled.img`
   border-radius: 20px;
   max-height: 228px;
   object-fit: cover;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px;
   top: 20px;
   left: 50px;
 `;
