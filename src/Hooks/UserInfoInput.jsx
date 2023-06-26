3f9a1320f9b12d1c5031dbc2d30ea46cc5ab915e
Da-Youn <dayoun202@gmail.com>
2023-06-26 15:40:30 +0900

    [🔨 Refactor] 이미지 오류 수정, 불필요한 import 코드 제거

--- src/Hooks/UserInfoInput.jsx
+++ src/Hooks/UserInfoInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export default function UserInfoInput(button, disabledButton) {
   const [email, setEmail] = useState('');
