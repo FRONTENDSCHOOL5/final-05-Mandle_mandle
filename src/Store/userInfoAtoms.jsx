ae79d8bb00e318c5396ed7931ec4055143053f61
junny97 <pcohad12@gmail.com>
2023-08-27 18:58:57 +0900

    [✨feat] 자동 로그인 상태 atom 추가 #205

--- src/Store/userInfoAtoms.jsx
+++ src/Store/userInfoAtoms.jsx
@@ -1,16 +1,22 @@
-import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist();
 // 로그인된 유저 정보 저장
 export const UserAtom = atom({
-  key: "UserAtom",
+  key: 'UserAtom',
   default: {},
   effects_UNSTABLE: [persistAtom],
 });
 
 export const IsLogin = atom({
-  key: "IsLogin",
+  key: 'IsLogin',
+  default: false,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const AutoLogin = atom({
+  key: 'AutoLogin',
   default: false,
   effects_UNSTABLE: [persistAtom],
 });
