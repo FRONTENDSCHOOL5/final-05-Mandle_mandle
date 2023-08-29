249023e034926ec05c335a6ef544eb8422c00818
5647kr <5647kr@gmail.com>
2023-08-30 00:43:16 +0900

    [✨	Feat] 클래스 페이지 스크롤 기능 추가 1

--- src/pages/Class/Class.jsx
+++ src/pages/Class/Class.jsx
@@ -1,62 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 import { UserAtom } from '../../Store/userInfoAtoms';
 import { HomeNav } from '../../components/Common/TopNav';
 import { ClassPost, ClassPostMini } from '../../components/Common/ClassPost';
 import MenuBar from '../../components/Common/MenuBar';
 import { Link } from 'react-router-dom';
-import {
-  HiddenContext,
-  MainWrap,
-  MiniSection,
-  ClassSection,
-  Title,
-  MiniList,
-  ClassList,
-} from './ClassStyle';
+import { HiddenContext, MainWrap, MiniSection, ClassSection, Title, MiniList, ClassList } from './ClassStyle';
 import GetClassData from '../../api/GetClassData';
 import ClassSkeleton from '../../components/Common/Skeleton/ClassSkeleton';
+
 export default function Class() {
   const UserInfo = useRecoilValue(UserAtom);
   const token = UserInfo.token;
   const [loading, setLoading] = useState(true);
   const [popularClasses, setPopularClasses] = useState([]);
   const [newClasses, setNewClasses] = useState([]);
+  const [page, setPage] = useState(1); // Track the current page
+
+  const mainWrapRef = useRef(null); // Create a ref for the MainWrap element
+
+  const handleScroll = () => {
+    if (mainWrapRef.current) {
+      const bottom = mainWrapRef.current.scrollHeight - mainWrapRef.current.scrollTop === mainWrapRef.current.clientHeight;
+
+      if (bottom) {
+        setPage(page + 1); // Increment the page
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await GetClassData(token);
-        const filteredClasses = data.product.filter((classItem) =>
-          classItem.author.accountname.includes('Teacher'),
-        );
-        console.log(filteredClasses);
-        const popularClasses = filteredClasses.slice(0, 3);
-        setPopularClasses(popularClasses);
-        setNewClasses(filteredClasses);
+        const data = await GetClassData(token, page);
+        const filteredClasses = data.product.filter(classItem => classItem.author.accountname.includes('Teacher'));
+
+        if (page === 1) {
+          setPopularClasses(filteredClasses.slice(0, 3));
+          setNewClasses(filteredClasses.slice(0, 8));
+        } else {
+          const startIndex = (page - 2) * 2 + 8; // Calculate the starting index
+          setNewClasses(prevClasses => [
+            ...prevClasses,
+            ...filteredClasses.slice(startIndex, startIndex + 2) // Add 2 new classes
+          ]);
+        }
+
         setLoading(false);
       } catch (error) {
-        console.log('Error', error);
+        console.log("Error", error);
         setLoading(false);
       }
     };
 
     fetchData();
-  }, [token]);
+
+    if (mainWrapRef.current) {
+      mainWrapRef.current.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (mainWrapRef.current) {
+        mainWrapRef.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [token, page]);
+
   return (
     <>
       <HomeNav title={'클래스'}>
         <HiddenContext>클래스 피드</HiddenContext>
       </HomeNav>
-      <MainWrap>
-        {loading ? (
-          <ClassSkeleton />
-        ) : (
+      <MainWrap ref={mainWrapRef}>
+        {loading ? (<ClassSkeleton />) : (
           <>
             <MiniSection>
               <Title>인기 클래스</Title>
               <MiniList>
-                {popularClasses.map((classItem) => (
+                {popularClasses.map(classItem => (
                   <li key={classItem._id}>
                     <Link to={`/class/detail/${classItem._id}`}>
                       <ClassPostMini
@@ -73,7 +94,7 @@ export default function Class() {
             <ClassSection>
               <Title>새로운 클래스</Title>
               <ClassList>
-                {newClasses.map((classItem) => (
+                {newClasses.map(classItem => (
                   <li key={classItem._id}>
                     <Link to={`/class/detail/${classItem._id}`}>
                       <ClassPost
