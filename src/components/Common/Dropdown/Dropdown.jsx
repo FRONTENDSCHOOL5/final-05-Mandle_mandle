347ffbaeca0b4ab825793bd72ae02f560c30a502
junny97 <pcohad12@gmail.com>
2023-10-04 16:30:09 +0900

    [🐛Feat] 드롭다운 메뉴에 해당 클래스 이미지 추가 #251

--- src/components/Common/Dropdown/Dropdown.jsx
+++ src/components/Common/Dropdown/Dropdown.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Dropdown(props) {
-  const { value, setClassIdentify, img, setIsOpen, isOpen } = props;
-  const ValueClick = () => {
-    setClassIdentify(value);
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <>
-      <ImageBox img={img} />
-      <DropdownItem onClick={ValueClick}>{value}</DropdownItem>
-    </>
-  );
-}
-
-export { DropdownContainer, DropdownButton, DropdownMenu, ImageBox };
+const DropdownContainer = styled.div`
+  width: 358px;
+  height: 72px;
+  border: 1px solid var(--sub-font-color);
+  position: relative;
+  margin: 0 auto;
+  border-radius: 8px;
+  margin-top: 16px;
+`;
 
 const DropdownItem = styled.li`
   padding: 10px;
@@ -24,14 +17,6 @@ const DropdownItem = styled.li`
   &:hover {
     background-color: #f7f7f7;
   }
-`;
-
-const DropdownContainer = styled.div`
-  width: 358px;
-  height: 72px;
-  border: 1px solid #ccc;
-  position: relative;
-  margin: 0 auto;
 `;
 
 const DropdownButton = styled.button`
@@ -55,8 +40,23 @@ const ImageBox = styled.img`
   width: 52px;
   object-fit: cover;
   height: 52px;
-
   border: 1px solid #ccc;
-  border-radius: 4px; // 필요에 따라 추가한 부분입니다. 원하지 않으면 삭제할 수 있습니다.
-  margin: 10px; // 적절한 간격을 위해 추가하였습니다. 조절이 필요할 수 있습니다.
+  border-radius: 4px;
+  margin: 10px;
 `;
+
+export default function Dropdown(props) {
+  const { value, setClassIdentify, img, setIsOpen, isOpen } = props;
+  const ValueClick = () => {
+    setClassIdentify(value);
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <ImageBox src={img} />
+      <DropdownItem onClick={ValueClick}>{value}</DropdownItem>
+    </>
+  );
+}
+
+export { DropdownContainer, DropdownButton, DropdownMenu, ImageBox };
