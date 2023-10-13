4caf50e28922025587bdb59575ce87788e17233e
Da-Youn <dayoun202@gmail.com>
2023-10-13 16:56:56 +0900

    [🐛 Fix] netltfy 배포 후 렌더링 오류  - package.json homepage

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   return (
     <Wrap>
       <GlobalStyle />
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Suspense fallback={<div>로딩중입니다...</div>}>
           <Routes>
             <Route path='/' element={<Splash />} />
